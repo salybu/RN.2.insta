ed7e09ffaf33c1ab4403d96490180900221e2123
salybu <rkdmsdl93@gmail.com>
2022-04-22 09:51:42 +0900

    feat: use some navigator props (#14.2)

--- navigators/LoggedOutNav.js
+++ navigators/LoggedOutNav.js
@@ -8,9 +8,24 @@ const Stack = createNativeStackNavigator();
 
 export default function LoggedOutNav() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={Welcome} />
-      <Stack.Screen name="Login" component={Login} />
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerTintColor: "black",
+      }}
+    >
+      <Stack.Screen
+        name="Welcome"
+        options={{
+          title: "Welcome babe",
+        }}
+        component={Welcome}
+      />
+      <Stack.Screen
+        name="Login"
+        options={{ headerTintColor: "red" }}
+        component={Login}
+      />
       <Stack.Screen name="CreateAccount" component={CreateAccount} />
     </Stack.Navigator>
   );
